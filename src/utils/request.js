787357d0125ae8ae0d110b18232f5ff4857c2983
Layne <fdgod5@163.com>
2022-11-09 09:07:55 +0800

    将request和用户模块的代码进行清理

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,85 +1,92 @@
-import axios from 'axios'
-import { MessageBox, Message } from 'element-ui'
-import store from '@/store'
-import { getToken } from '@/utils/auth'
+// 导出一个axios的实例，而且这个十来要有请求拦截器 响应拦截器
+import axios from "axios";
+const service = axios.create(); // 创建一个axios的实例
+service.interceptors.request.use(); // 请求拦截器
+service.interceptors.response.use(); // 响应拦截器
+export default service; // 导出axios实例
 
-// create an axios instance
-const service = axios.create({
-  baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
-  // withCredentials: true, // send cookies when cross-domain requests
-  timeout: 5000 // request timeout
-})
+// import axios from 'axios'
+// import { MessageBox, Message } from 'element-ui'
+// import store from '@/store'
+// import { getToken } from '@/utils/auth'
 
-// request interceptor
-service.interceptors.request.use(
-  config => {
-    // do something before request is sent
+// // create an axios instance
+// const service = axios.create({
+//   baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
+//   // withCredentials: true, // send cookies when cross-domain requests
+//   timeout: 5000 // request timeout
+// })
 
-    if (store.getters.token) {
-      // let each request carry token
-      // ['X-Token'] is a custom headers key
-      // please modify it according to the actual situation
-      config.headers['X-Token'] = getToken()
-    }
-    return config
-  },
-  error => {
-    // do something with request error
-    console.log(error) // for debug
-    return Promise.reject(error)
-  }
-)
+// // request interceptor
+// service.interceptors.request.use(
+//   config => {
+//     // do something before request is sent
 
-// response interceptor
-service.interceptors.response.use(
-  /**
-   * If you want to get http information such as headers or status
-   * Please return  response => response
-  */
+//     if (store.getters.token) {
+//       // let each request carry token
+//       // ['X-Token'] is a custom headers key
+//       // please modify it according to the actual situation
+//       config.headers['X-Token'] = getToken()
+//     }
+//     return config
+//   },
+//   error => {
+//     // do something with request error
+//     console.log(error) // for debug
+//     return Promise.reject(error)
+//   }
+// )
 
-  /**
-   * Determine the request status by custom code
-   * Here is just an example
-   * You can also judge the status by HTTP Status Code
-   */
-  response => {
-    const res = response.data
+// // response interceptor
+// service.interceptors.response.use(
+//   /**
+//    * If you want to get http information such as headers or status
+//    * Please return  response => response
+//   */
 
-    // if the custom code is not 20000, it is judged as an error.
-    if (res.code !== 20000) {
-      Message({
-        message: res.message || 'Error',
-        type: 'error',
-        duration: 5 * 1000
-      })
+//   /**
+//    * Determine the request status by custom code
+//    * Here is just an example
+//    * You can also judge the status by HTTP Status Code
+//    */
+//   response => {
+//     const res = response.data
 
-      // 50008: Illegal token; 50012: Other clients logged in; 50014: Token expired;
-      if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
-        // to re-login
-        MessageBox.confirm('You have been logged out, you can cancel to stay on this page, or log in again', 'Confirm logout', {
-          confirmButtonText: 'Re-Login',
-          cancelButtonText: 'Cancel',
-          type: 'warning'
-        }).then(() => {
-          store.dispatch('user/resetToken').then(() => {
-            location.reload()
-          })
-        })
-      }
-      return Promise.reject(new Error(res.message || 'Error'))
-    } else {
-      return res
-    }
-  },
-  error => {
-    console.log('err' + error) // for debug
-    Message({
-      message: error.message,
-      type: 'error',
-      duration: 5 * 1000
-    })
-    return Promise.reject(error)
-  }
-)
+//     // if the custom code is not 20000, it is judged as an error.
+//     if (res.code !== 20000) {
+//       Message({
+//         message: res.message || 'Error',
+//         type: 'error',
+//         duration: 5 * 1000
+//       })
 
-export default service
+//       // 50008: Illegal token; 50012: Other clients logged in; 50014: Token expired;
+//       if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
+//         // to re-login
+//         MessageBox.confirm('You have been logged out, you can cancel to stay on this page, or log in again', 'Confirm logout', {
+//           confirmButtonText: 'Re-Login',
+//           cancelButtonText: 'Cancel',
+//           type: 'warning'
+//         }).then(() => {
+//           store.dispatch('user/resetToken').then(() => {
+//             location.reload()
+//           })
+//         })
+//       }
+//       return Promise.reject(new Error(res.message || 'Error'))
+//     } else {
+//       return res
+//     }
+//   },
+//   error => {
+//     console.log('err' + error) // for debug
+//     Message({
+//       message: error.message,
+//       type: 'error',
+//       duration: 5 * 1000
+//     })
+//     return Promise.reject(error)
+//   }
+// )
+
+// export default service
